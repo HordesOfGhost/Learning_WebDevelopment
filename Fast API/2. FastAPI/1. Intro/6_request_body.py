e6c34db0d0af79b1dcf9d0c2e5c548d16fb0d578
bibek-icebrkr <bibek.thapa@icebrkr.ai>
2024-08-12 18:12:48 +0545

    latest push

--- Fast API/2. FastAPI/1. Intro/6_request_body.py
+++ Fast API/2. FastAPI/1. Intro/6_request_body.py
@@ -15,4 +15,24 @@ app = FastAPI()
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_wit_tax": price_with_tax})
+    return item_dict
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     item.name = item.name.capitalize()
+#     item.description = item.description.title()
+#     return {"item_id": item_id, **item.model_dump()}
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q :Union[str, None]=None):
+
+    item.name = item.name.capitalize()
+    item.description = item.description.title()
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q":q})
+    return result
